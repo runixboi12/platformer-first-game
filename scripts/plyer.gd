f795035eeddf0af4e685e6f1571413d95d9119e2
Anmol Robindabank <nagiyalanmol@gmail.com>
2026-01-03 12:27:57 +0400

    added basic pysics and initialized animations in code

--- scripts/plyer.gd
+++ scripts/plyer.gd
@@ -1,25 +1,38 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -850.0
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	#Add the animations
+	if velocity.x>1 or velocity.x<-1:
+		animated_sprite_2d.animation = "RUN"
+	else:
+		animated_sprite_2d.animation="idle"
+			
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		animated_sprite_2d.animation="JUMP"
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	if direction == 1.0:
+		animated_sprite_2d.flip_h = false
+	elif direction == -1.0:
+		animated_sprite_2d.flip_h = true
